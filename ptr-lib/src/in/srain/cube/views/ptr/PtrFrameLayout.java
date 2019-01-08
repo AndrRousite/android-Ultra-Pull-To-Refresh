0d7c879e82749f413ffcddf22c37262eb43f3aee
刘枫sky1234 <13766398+AndrRousite@users.noreply.github.com>
2019-01-08 14:57:54 +0800

    Update PtrFrameLayout.java

--- ptr-lib/src/in/srain/cube/views/ptr/PtrFrameLayout.java
+++ ptr-lib/src/in/srain/cube/views/ptr/PtrFrameLayout.java
@@ -106,8 +106,8 @@ public class PtrFrameLayout extends ViewGroup {
 
         mScrollChecker = new ScrollChecker();
 
-        final ViewConfiguration conf = ViewConfiguration.get(getContext());
-        mPagingTouchSlop = conf.getScaledTouchSlop() * 2;
+        //final ViewConfiguration conf = ViewConfiguration.get(getContext());
+        //mPagingTouchSlop = conf.getScaledTouchSlop() * 2;
     }
 
     @Override
